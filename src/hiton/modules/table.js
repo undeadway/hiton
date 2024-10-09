c8a29dd8cb3f73d655f9bdca9cf29f20cca20e4d
weigucheng <weigucheng@i-hc.cn>
2024-10-09 14:24:58 +0800

    改回 Array.forEach 形式

--- src/hiton/modules/table.js
+++ src/hiton/modules/table.js
@@ -14,14 +14,11 @@ const replaceTable = (input) => {
 		let part = matches[0]
 		let output = [];
 
-        const arr = part.split(Space.LF);
-        for (let i = 0, len = arr.length; i < len; i++) {
-			if (i === 1) return;
-            let line = arr[i];
+        Array.forEach(part.split(Space.LF), (i, line) => {
 			if (String.isEmpty(line)) return;
 			line = line.slice(1, line.length - 1); // 去掉最开始和最后的 |
 			output.push(TD_START + line.replace(VERTICAL_BAR, TD_JOIN) + TD_END);
-        }
+        });
 
 		let table = TABLE_START + output.join(TR_JOIN) + TABLE_END;
 
