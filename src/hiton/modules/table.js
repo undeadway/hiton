164c2c72949ffe5ab194fd3e64629f51057a02cd
weigucheng <weigucheng@i-hc.cn>
2024-10-10 14:43:45 +0800

    添加表格处理

--- src/hiton/modules/table.js
+++ src/hiton/modules/table.js
@@ -1,6 +1,10 @@
+const basicReplace = require("./inline");
+
 const { Char: { Space } } =JsConst;
-const TABLE_REGEX = /(\|(.)+\|\n)+/,
-	VERTICAL_BAR = /\|/g;
+const TABLE_REGX = /(\|(.)+\|\n)+/,
+	VERTICAL_BAR = /\|/g,
+	TABLE_DEFINE_REGX = /@\{(.+)\}/,
+	TABLE_CALLING_REGX =/@:\{(.+)\}/;
 const TR_JOIN = "</tr><tr>",
 	TD_JOIN = "</td><td>",
 	TD_START = "<td>",
@@ -9,38 +13,66 @@ const TR_JOIN = "</tr><tr>",
 	THEAD_END = "</tr></thead>",
 	TBODY_START = "<tbody><tr>",
 	TBODY_END = "</tr></tbody>",
-	TABLE_START = `<table class="table">`,
+	TABLE_START = `<table class="hiton-table">`,
 	TABLE_END = "</table>";
 
-const replaceTable = (hitOn, input) => {
+const replaceTable = (hitOn) => {
 
-	while ((matches = input.match(TABLE_REGEX)) !== null) {
-		const part = matches[0], output = [];
-		let tHead = "";
+	const tableMapping = {};
+	let tableCount = 1;
 
-        Array.forEach(part.split(Space.LF), (index, line) => {
-			if (String.isEmpty(line)) return;
-			line = line.slice(1, line.length - 1); // 去掉最开始和最后的 |
-			line = line.split(VERTICAL_BAR).map(td => {
-				td = td.trim();
-				td = hitOn.parse(td);
-				return td;
-			});
-			if (index === 0) {
-				tHead = THEAD_START + TD_START + line.join(TD_JOIN) + TD_END + THEAD_END;
-			} else if (index !== 1) {
-				output.push(TD_START + line.join(TD_JOIN) + TD_END);
-			}
-        });
-
-		const tBody = TBODY_START + output.join(TR_JOIN) + TBODY_END;
-
-		let table = TABLE_START + tHead + tBody + TABLE_END;
-
-		input = input.replace(part, table);
+	function calcMapCount(name) {
+		let count = tableMapping[name];
+		if (!count) {
+			count = tableMapping[name] = tableCount++;
+		}
+		return count;
 	}
 
-	return input;
+	return (input) => {
+		// 表格
+		while ((matches = input.match(TABLE_REGX)) !== null) {
+			const part = matches[0], output = [];
+			let tHead = "";
+
+			Array.forEach(part.split(Space.LF), (index, line) => {
+				if (String.isEmpty(line)) return;
+				line = line.slice(1, line.length - 1); // 去掉最开始和最后的 |
+				line = line.split(VERTICAL_BAR).map(td => {
+					td = td.trim();
+					td = basicReplace(td); // 递归调用
+					return td;
+				});
+				if (index === 0) {
+					tHead = THEAD_START + TD_START + line.join(TD_JOIN) + TD_END + THEAD_END;
+				} else if (index !== 1) {
+					output.push(TD_START + line.join(TD_JOIN) + TD_END);
+				}
+			});
+
+			let table = TABLE_START + tHead + TBODY_START + output.join(TR_JOIN) + TBODY_END; + TABLE_END;
+			input = input.replace(part, table);
+		}
+
+		// 表格声明
+		while ((matched = TABLE_DEFINE_REGX.exec(input)) !== null) {
+			const [ proto, name ] = matched;
+			const count = calcMapCount(name);
+
+			const output = `<div id="hiton-table-id__${count}" class="hiton-table-define">表：${name}</div>`;
+			input = input.replace(proto, output);
+		}
+		// 表格引用
+		while ((matched = TABLE_CALLING_REGX.exec(input)) !== null) {
+			const [ proto, name ] = matched;
+			const count = calcMapCount(name);
+
+			const output = `<a class="hiton-span-bold hiton-span-margin hiton-table-calling" href="#hiton-table-id__${count}">表：${name}</a>`;
+			input = input.replace(proto, output);
+		}
+
+		return input;
+	}
 }
 
 module.exports = exports = replaceTable;
