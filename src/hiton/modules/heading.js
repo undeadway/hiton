4cb1321d97808c87f074355943aeb0e90d1282e0
weigucheng <weigucheng@i-hc.cn>
2024-10-10 15:33:08 +0800

    添加区分计数模式和普通模式

--- src/hiton/modules/heading.js
+++ src/hiton/modules/heading.js
@@ -1,27 +1,69 @@
-const H6_REGX = /###### (.*?)(\n|$)/g,
-    H5_REGX = /##### (.*?)(\n|$)/g,
-    H4_REGX = /#### (.*?)(\n|$)/g,
-    H3_REGX = /### (.*?)(\n|$)/g,
-    H2_REGX = /## (.*?)(\n|$)/g,
-    H1_REGX = /# (.*?)(\n|$)/g;
-const H6_STR = "<h6 class=\"h6\">$1</h6>",
-    H5_STR = "<h5 class=\"h5\">$1</h5>",
-    H4_STR = "<h4 class=\"h4\">$1</h4>",
-    H3_STR = "<h3 class=\"h3\">$1</h3>",
-    H2_STR = "<h2 class=\"h2\">$1</h2>",
-    H1_STR = "<h1 class=\"h1\">$1</h1>";
+const { Char } = JsConst;
 
-function headingReplace () {
+const BASIC_H6_REGX = /###### (.*?)(\n|$)/g,
+    BASIC_H5_REGX = /##### (.*?)(\n|$)/g,
+    BASIC_H4_REGX = /#### (.*?)(\n|$)/g,
+    BASIC_H3_REGX = /### (.*?)(\n|$)/g,
+    BASIC_H2_REGX = /## (.*?)(\n|$)/g,
+    BASIC_H1_REGX = /# (.*?)(\n|$)/g,
+    COUNTING_H_REGX = /\n(#{1,6}) (.*?)\n/;
+    const BASIC_H6_STR = "<h6>$1</h6>",
+    BASIC_H5_STR = "<h5>$1</h5>",
+    BASIC_H4_STR = "<h4>$1</h4>",
+    BASIC_H3_STR = "<h3>$1</h3>",
+    BASIC_H2_STR = "<h2>$1</h2>",
+    BASIC_H1_STR = "<h1>$1</h1>";
+
+
+const COUNTING_MODE = "COUNTING-MODE";
+
+function basicMode (input) {
+    input = input.replace(BASIC_H6_REGX, BASIC_H6_STR); // 六级标题
+    input = input.replace(BASIC_H5_REGX, BASIC_H5_STR); // 五级标题
+    input = input.replace(BASIC_H4_REGX, BASIC_H4_STR); // 四级标题
+    input = input.replace(BASIC_H3_REGX, BASIC_H3_STR); // 三级标题
+    input = input.replace(BASIC_H2_REGX, BASIC_H2_STR); // 二级标题
+    input = input.replace(BASIC_H1_REGX, BASIC_H1_STR); // 一级标题
+
+    return input;
+}
+
+function highMode() {
+
+    let levelIndex = [ 0, 0, 0, 0, 0, 0 ], lastLevel = 0;
 
     return (input) => {
-        input = input.replace(H6_REGX, H6_STR); // 六级标题
-        input = input.replace(H5_REGX, H5_STR); // 五级标题
-        input = input.replace(H4_REGX, H4_STR); // 四级标题
-        input = input.replace(H3_REGX, H3_STR); // 三级标题
-        input = input.replace(H2_REGX, H2_STR); // 二级标题
-        input = input.replace(H1_REGX, H1_STR); // 一级标题
-    
+        while ((matched = COUNTING_H_REGX.exec(input)) !== null) {
+            let [ proto, level, text ] = matched;
+            level = level.length;
+            text = text.trim();
+
+            if (lastLevel > level) {
+				for (let i = level; i < levelIndex.length; i++) {
+					levelIndex[i] = 0;
+				}
+			}
+	
+			levelIndex[level - 1]++;
+            const thisLevel = levelIndex.slice(0, level);
+
+			const chapter = thisLevel.join(Char.POINT);
+            const id = thisLevel.join(Char.UNDER_SOURCE);
+			lastLevel = level;
+	
+			const output = `\n<p id="hiton-heading__${id}" class="hiton-counting-mode-heading">${chapter}. ${text}</p>\n`;
+			input = input.replace(proto, output);
+        }
+
         return input;
+    }
+}
+
+function headingReplace (options) {
+    if (options.heading.toUpperCase() === COUNTING_MODE) {
+        return  highMode();
+    } else {
+        return basicMode;
     }
 }
 
