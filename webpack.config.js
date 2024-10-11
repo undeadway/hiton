be6180930c3af28e97ae5d0176aad25682773074
weigucheng <weigucheng@i-hc.cn>
2024-10-11 11:53:46 +0800

    添加 webpack 打包

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+	entry: { "hiton": "./src/index.js" },
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].js"
+	},
+	mode: 'development'
+};
