12d52134ce5279decbd4d25889a9c3fc0f5e7b5f
weigucheng <weigucheng@i-hc.cn>
2024-10-14 13:11:11 +0800

    调整顺序

--- src/modules/codes.js
+++ src/modules/codes.js
@@ -6,16 +6,16 @@ const CODES_REGX = /\n```(.*)\n([^`]+?)```\n/;
 function replaceCodes (options) {
 
 	codes.before = input => {
-		if (!options.codes) {
-			return input;
-		} else {
+		if (options.codes) {
 			while ((matched = CODES_REGX.exec(input)) !== null) {
 				let [ proto, lang, code ] = matched;
-				code = options.codes(code, lang);
+				code = options.codes(code, lang); // 代码需要依赖第三方的代码解析器来实现
 				code = `<pre class="language-html">${code}</pre>`;
 
 				input = codes.replace(input, proto, code);
 			}
+			return input;
+		} else {
 			return input;
 		}
 	}
